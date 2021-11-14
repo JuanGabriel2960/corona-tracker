d4eb2ab9b3292a0fa4f5dd448bfd899356354798
Juan Gabriel Tavárez López <jtavarez@intellisys.com.do>
2021-11-14 15:46:07 -0400

    fix: updated heroku deploy scripts

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/corona-tracker'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/corona-tracker/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-//Path Location Strategy
-
-app.get('/*', function(req, res) {
-res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
-
-console.log('Console listening!');
